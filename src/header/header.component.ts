8f78931cbdb5f7bcc7ddb3ca8f7cbdfb5a35df54
siysitho022 <siysitho022@student.wethinkcode.co.za>
2024-05-28 14:47:04 +0200

    Refactored project structure: [Separated component files]

---
+++ src/header/header.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component'; 
+
+// @Component({
+//   selector: 'app-header',
+//   standalone: true,
+//   imports: [MenuComponent],
+//   // template: `        #Un-Comment to change page details for ne in-line Template
+//   //   <h1>My Header</h1>
+//   //   <b>HeaderComponent</b>
+//   // `,
+//   template: '<app-menu/>',
+//   styleUrl: './header.component.css'
+// })
+// export class HeaderComponent {
+
+// }
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MenuComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {
+  name = 'Calab';
+}
