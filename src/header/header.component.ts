a9fb3cc7100b47086aa11580f917c15f9c06e84f
siysitho022 <siysitho022@student.wethinkcode.co.za>
2024-05-29 07:50:14 +0200

    added new header component controller

--- src/header/header.component.ts
+++ src/header/header.component.ts
@@ -5,7 +5,7 @@ import { MenuComponent } from '../menu/menu.component';
 //   selector: 'app-header',
 //   standalone: true,
 //   imports: [MenuComponent],
-//   // template: `        #Un-Comment to change page details for ne in-line Template
+//   // template: `        //Un-Comment to change page details for ne in-line Template
 //   //   <h1>My Header</h1>
 //   //   <b>HeaderComponent</b>
 //   // `,
@@ -16,13 +16,24 @@ import { MenuComponent } from '../menu/menu.component';
 
 // }
 
+// @Component({
+//   selector: 'app-header',
+//   standalone: true,
+//   imports: [MenuComponent],
+//   templateUrl: './header.component.html',
+//   styleUrl: './header.component.css',
+// })
+// export class HeaderComponent {
+//   name = 'Calab';
+// }
+
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MenuComponent],
-  templateUrl: './header.component.html',
+  // template: '<h1>My Notes</h1>',
+  template: '<h1>{{ title }}</h1>', //comment out to use interpolation
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  name = 'Calab';
+  title = 'My Notes'
 }
